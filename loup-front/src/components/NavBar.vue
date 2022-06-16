ec55a18d6028a3e76b1e8a4ccb5930c9511d635b
@Davi_Nascimento <davi.silva@salesoft.com.br>
2022-06-16 20:10:03 -0300

    Perfil nome

--- loup-front/src/components/NavBar.vue
+++ loup-front/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="navbar" class="navbar navbar-expand-md navbar-dark bg-dark mb-4 nvabarra">
   <div class="container-fluid">
-    <router-link class="nav-link active" to="/perfil"><img src="@/assets/img/Logo.png" alt="twbs" width="40" height="40" style="border: 1px solid black;" class="rounded-circle flex-shrink-0"/></router-link> 
+    <router-link class="nav-link active" to="/perfil"><img src="@/assets/img/Logo.png" alt="twbs" width="40" height="40" style="border: 1px solid black;" class="rounded-circle flex-shrink-0"/>Perfil</router-link> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
